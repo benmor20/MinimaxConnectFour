4f1bc0fba0bfcbf27b7b5476c841dc445c5dfa33
benmor20 <benmor2020@gmail.com>
2022-10-30 12:48:13 -0400

    Start creation of Tree

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """
 Main run script
 """
+import itertools
 
 from model import ConnectFour
 from view import TextView
 from controller import PlayerController, MinimaxController
+from minimax import Tree, _minimax
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -12,25 +16,30 @@ def main():
     The main algorithm for Connect Four
     """
 
-    # Initialize MVC
+    # # Initialize MVC
+    # board = ConnectFour()
+    # view = TextView(board)
+    # p1 = PlayerController(board, True)
+    # p2 = MinimaxController(board, False)
+    #
+    # view.draw()
+    # while True:
+    #     # P1 Move
+    #     p1.move()
+    #     view.draw()
+    #     if board.check_win() == 1:
+    #         break
+    #
+    #     # P2 Move
+    #     p2.move()
+    #     view.draw()
+    #     if board.check_win() == -1:
+    #         break
+
     board = ConnectFour()
-    view = TextView(board)
-    p1 = PlayerController(board, True)
-    p2 = MinimaxController(board, False)
-
-    view.draw()
-    while True:
-        # P1 Move
-        p1.move()
-        view.draw()
-        if board.check_win() == 1:
-            break
-
-        # P2 Move
-        p2.move()
-        view.draw()
-        if board.check_win() == -1:
-            break
+    tree = Tree(board)
+    _minimax(2, board, None, True, tree)
+    tree.display()
 
 
 if __name__ == '__main__':
--- minimax.py
+++ minimax.py
@@ -6,21 +6,102 @@ from model import ConnectFour, WIN_KERNELS
 from scipy import signal
 import numpy as np
 from typing import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def minimax(depth: int, gamestate: ConnectFour, maximize: bool) -> int:
+class Tree:
+    """
+    Class to represent the tree-like structure of minimax
+
+    Attributes:
+        gamestate: the ConnectFour state this branch of the tree represents
+        score: an int, the score of the gamestate, or None if no score is calculated
+        children: a list of Trees, the possible children of this gamestate, each index corresponding to placing a token
+            in the corresponding column.
+    """
+    def __init__(self, gamestate: ConnectFour, score: Optional[int] = None):
+        self.gamestate = gamestate
+        self.score = score
+        self.children: List[Optional[Tree]] = [None for _ in range(self.gamestate.board.shape[1])]
+
+    def nodes(self) -> Iterable['Tree']:
+        """
+        :return: a Generator of all the Trees at or below this, starting with self, then continuing depth-first
+        """
+        yield self
+        for c in self.children:
+            if c is not None:
+                yield from c.nodes()
+
+    def connect_graph(self, graph: nx.Graph):
+        """
+        Makes the connections in a networkx Graph between this node and its children
+
+        :param graph: the Graph to make the connections on
+        """
+        for c in self.children:
+            if c is not None:
+                graph.add_edge(self, c)
+                c.connect_graph(graph)
+
+    def make_graph(self) -> nx.Graph:
+        """
+        Create a graph representing this tree
+
+        :return: a Graph representation of this Tree, where each node is an instance of Tree
+        """
+        graph = nx.Graph()
+        graph.add_nodes_from(self.nodes())
+        self.connect_graph(graph)
+        return graph
+
+    def display(self):
+        """
+        Displays this tree
+        """
+        graph = self.make_graph()
+        nx.draw(graph)
+        plt.show()
+
+    def __setitem__(self, key: int, value: 'Tree'):
+        """
+        Sets the given child to the given Tree
+
+        :param key: an int, the index of the child to set
+        :param value: a Tree, the value to set the child to
+        """
+        self.children[key] = value
+
+    def __getitem__(self, item) -> Optional['Tree']:
+        """
+        Gets the given child
+
+        :param item: an int, the index of the child to get
+        :return: a Tree, the child at the given index
+        """
+        return self.children[item]
+
+
+def minimax(depth: int, gamestate: ConnectFour, maximize: bool, make_tree: bool = False) -> int:
     """
     Performs the minimax algorithm on the current gamestate
 
     :param depth: an int that describes the maximum look depth
     :param gamestate: an instance of ConnectFour
     :param maximize: a bool representing the maximizing (True) or minimizing (False) player.
+    :param make_tree: a bool, whether to create and display the minimax Tree
     :return: The optimal column to play according to minimax
     """
-    return _minimax(depth, gamestate, None, maximize)[1]
+    tree = Tree(gamestate) if make_tree else None
+    column = _minimax(depth, gamestate, None, maximize, tree)[1]
+    if make_tree:
+        tree.display()
+    return column
 
 
-def minimaxab(depth: int, gamestate: ConnectFour, alpha: int, beta: int, maximize: bool) -> int:
+def minimaxab(depth: int, gamestate: ConnectFour, alpha: int, beta: int, maximize: bool, make_tree: bool = False)\
+        -> int:
     """
     Performs the minimax algorithm on a given gamestate, with Alpha-Beta pruning
 
@@ -29,12 +110,18 @@ def minimaxab(depth: int, gamestate: ConnectFour, alpha: int, beta: int, maximiz
     :param alpha: the Alpha parameter for AB pruning
     :param beta: the Beta parameter for AB pruning
     :param maximize: a bool representing the maximizing (True) or minimizing (False) player.
+    :param make_tree: a bool, whether to create and display the minimax Tree
     :return: The optimal column to play according to minimax with AB pruning
     """
-    return _minimax(depth, gamestate, (alpha, beta), maximize)[1]
+    tree = Tree(gamestate) if make_tree else None
+    column = _minimax(depth, gamestate, (alpha, beta), maximize, tree)[1]
+    if make_tree:
+        tree.display()
+    return column
 
 
-def _minimax(depth: int, gamestate: ConnectFour, ab: Optional[Tuple[int, int]], maximize: bool) -> Tuple[int, int]:
+def _minimax(depth: int, gamestate: ConnectFour, ab: Optional[Tuple[int, int]], maximize: bool, tree: Optional[Tree])\
+        -> Tuple[int, int]:
     """
     Performs the minimax algorithm on a given gamestate, with Alpha-Beta pruning
 
@@ -42,11 +129,15 @@ def _minimax(depth: int, gamestate: ConnectFour, ab: Optional[Tuple[int, int]], 
     :param gamestate: an instance of ConnectFour
     :param ab: a tuple containing the alpha and beta parameters for AB pruning, or None to perform regular minimax
     :param maximize: a bool representing the maximizing (True) or minimizing (False) player.
+    :param tree: the Tree of the given gamestate, or None to not create a Tree
     :return: A tuple of ints containing first the score then the column to get that score.
     """
     # Base case - reached minimum depth or someone has won
     if depth == 0 or gamestate.check_win() != 0:
-        return static_eval(gamestate), 0
+        score = static_eval(gamestate)
+        if tree is not None:
+            tree.score = score
+        return score, -1
 
     # Generate all possible next moves
     children = [gamestate.create_child(i) for i in range(7)]
@@ -55,7 +146,9 @@ def _minimax(depth: int, gamestate: ConnectFour, ab: Optional[Tuple[int, int]], 
     # For each child, perform minimax
     for i, child in enumerate(children):
         if child is not None:
-            score = _minimax(depth - 1, child, ab, not maximize)[0]
+            if tree is not None:
+                tree[i] = Tree(child)
+            score = _minimax(depth - 1, child, ab, not maximize, None if tree is None else tree[i])[0]
             # If the score is less or we are trying to maximize (but not both) then found new good score
             # Or if best[1] is -1, then this is our first run and we need to set it
             if (score < best[0] ^ maximize) or best[1] == -1:
@@ -71,6 +164,8 @@ def _minimax(depth: int, gamestate: ConnectFour, ab: Optional[Tuple[int, int]], 
                 ab = alpha, beta
                 if beta < alpha:
                     break
+    if tree is not None:
+        tree.score = best[0]
     return best
 
 
